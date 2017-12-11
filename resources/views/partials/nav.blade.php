6356109b5101783e0864a56287403be6ee49c9e6
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-11 18:32:25 -0500

    Document navbar, remove search form

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,20 +1,29 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">GoVice</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <a class="navbar-brand" href="/">GoVice</a>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
+        {{--Add spacing between site name and buttons --}}
+        <li class="nav-item active">
+            <a class="nav-link">       <span class="sr-only">(current)</span></a>
+        </li>
+        {{--Begin defining navbar buttons --}}
         @if (Route::has('login'))
             @auth
+            {{--Home--}}
             <li class="nav-item active">
                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
+            {{--Logout--}}
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('logout') }}">Logout <span class="sr-only">(current)</span></a>
+            </li>
             @else
+            {{--Login--}}
             <li class="nav-item active">
                 <a class="nav-link" href="{{ route('login') }}">Login <span class="sr-only">(current)</span></a>
             </li>
+            {{--Register--}}
             <li class="nav-item active">
                 <a class="nav-link" href="{{ route('register') }}">Register <span class="sr-only">(current)</span></a>
             </li>
@@ -22,9 +31,11 @@
         @endif
 
         </ul>
+        {{-- Remove search bar for now
         <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
+        --}}
     </div>
-    </nav>
+</nav>
