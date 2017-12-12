93056aa3eeb8c6756cc808e1310435d58b395b5a
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-11 19:23:25 -0500

    partialize templates

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,35 +1,28 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="/">GoVice</a>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-        {{--Add spacing between site name and buttons --}}
-        <li class="nav-item active">
-            <a class="nav-link">       <span class="sr-only">(current)</span></a>
-        </li>
-        {{--Begin defining navbar buttons --}}
-        @if (Route::has('login'))
+            <li class="nav-item active"> {{--Add spacing between site name and buttons --}}
+                <a class="nav-link"><span class="sr-only">(current)</span></a>
+            </li>
+            @if (Route::has('login')) {{--Begin defining navbar buttons --}}
             @auth
-            {{--Home--}}
-            <li class="nav-item active">
+            <li class="nav-item active">{{--Home--}}
                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
-            {{--Logout--}}
-            <li class="nav-item active">
+            <li class="nav-item active"> {{--Logout--}}
                 <a class="nav-link" href="{{ route('logout') }}">Logout <span class="sr-only">(current)</span></a>
             </li>
             @else
-            {{--Login--}}
-            <li class="nav-item active">
+            <li class="nav-item active"> {{--Login--}}
                 <a class="nav-link" href="{{ route('login') }}">Login <span class="sr-only">(current)</span></a>
             </li>
-            {{--Register--}}
-            <li class="nav-item active">
+            <li class="nav-item active"> {{--Register--}}
                 <a class="nav-link" href="{{ route('register') }}">Register <span class="sr-only">(current)</span></a>
             </li>
             @endauth
-        @endif
-
+            @endif
+            
         </ul>
         {{-- Remove search bar for now
         <form class="form-inline my-2 my-lg-0">
