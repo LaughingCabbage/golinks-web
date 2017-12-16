bbd3ad035a207af2ae51bb6cb1cc0415ffee85bf
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-16 17:49:28 -0500

    add store and show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use Session;
 
 class PostController extends Controller
 {
@@ -23,7 +25,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('post');
+        return view('posts.create');
     }
 
     /**
@@ -34,7 +36,21 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validate data
+        $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ));
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        Session::flash('success', 'Article Posted!');
+
+        return redirect()->route('post.show', $post->id);
+
     }
 
     /**
@@ -45,7 +61,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.show', compact('post'));
     }
 
     /**
