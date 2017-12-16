38c90c9f27c5b84f3a003c6794800f67134059f4
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-16 17:49:54 -0500

    add posts:create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,18 +2,22 @@
 
 @section('title', '| Create New Post')
 
+@push('stylesheets')
+ {{ Html::style('css/parsley.css') }}
+@endpush
+
 @section('content')
 
     <div class="row justify-content-md-center">
         <div class="col-md-8">
             <h1>Create New Blog Post</h1>
             <hr>
-            {!! Form::open(array('route' => 'post.store')) !!}
+            {!! Form::open(array('route' => 'post.store', 'data-parsley-validate' => '')) !!}
                 {{ Form::label('title', 'Title:') }}
-                {{ Form::text('title', null, array('class' => 'form-control')) }}
+                {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
 
                 {{ Form::label('body', "Body:") }}
-                {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+                {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
                 
                 {{ Form::submit('Create', array('class' => 'btn btn-success btn-lg btn-block')) }}
             {!! Form::close() !!}
@@ -21,3 +25,7 @@
     </div>
 
 @endsection
+
+@push('scripts')
+{{ Html::script('js/parsley.min.js') }}
+@endpush
