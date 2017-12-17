69342b2ad33e88dcf7a4d3c35d0b7bc381bdd278
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-17 16:12:56 -0500

    update blog show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,13 +2,44 @@
 
 @section('title', '| View Post')
 
+@push('stylesheets')
+{{ Html::style('css/blog.css') }}
+@endpush
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-md-center">
-        <div class="col-md-8">
-            <h1>{{ $post->title }}</h1>
-            <p class="lead">{{ $post->body }} </p>
+<div role="main" class="container">
+    <div class="row">
+        <div class="col-sm-8 blog-main">
+            <div class="blog-post">
+                <h1>{{ $post->title }}</h1>
+                <p class="lead">{{ $post->body }} </p>
+            </div>
+            @auth
+            <hr>
+      
+            <div class="col-sm-6">
+                <a href="#" class = "btn btn-primary btn-block">Edit</a>
+            </div>
+            <div class="col-sm-6">
+                <a href="#" class="btn btn-danger btn-block">Delete</a>
+            </div>
+            @endauth
         </div>
+
+        <aside class="col-sm-3 ml-sm-auto blog-sidebar">
+            <div class="sizebar-module">
+                <dl class="dl-horizontal">
+                    <dt>Created:</dt>
+                    <dd>{{ date_format($post->created_at, 'M j, Y') }}</dd>
+                </dl>
+                @if($post->created_at != $post->updated_at)
+                <dl class="dl-horizontal">
+                    <dt>Updated:</dt>
+                    <dd>{{ date_format($post->updated_at, 'M j, Y') }}</dd>
+                </dl> 
+                @endif
+            </div>
+        </aside>
     </div>
 </div>
 @endsection
