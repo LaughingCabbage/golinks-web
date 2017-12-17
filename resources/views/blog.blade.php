3a7b6cad2733cd5cbf549411a964549a2f38d83d
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-17 16:50:37 -0500

    display posts in table

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -16,4 +16,28 @@
             <a href="{{ route('post.create') }}" class="btn btn-lg btn-block btn-primary">Create Post</a>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table">
+                <thead>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Created</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach($posts as $post)
+                        <tr>
+                            <th>{{ $post->id }}</th>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->body }}</td>
+                            <td>{{ $post->created_at }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
