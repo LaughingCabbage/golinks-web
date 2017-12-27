a28ab333f652008fa7a64fecfa2dd67699c0c9ca
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-26 23:47:00 -0500

    render bootstrap locally

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,7 +5,7 @@
 
     <title>GoVice @yield('title')</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> <!-- Fonts -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}" crossorigin="anonymous"> <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     @yield('stylesheets')
 @stack('stylesheets')
