69bcd5a9490c462587c5c1ecaa02f296f8b1ee60
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-28 17:35:34 -0500

    add spacing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mt-5">
   <div class="row justify-content-center">
     <div class="col-md-10">
       <div class="card text-center">
@@ -17,8 +17,10 @@
           2 days ago
         </div>
       </div>
-      <hr class="cubes">
     </div>
+
+    <div class="col-md-10 mt-1"><hr class="cubes"></div>
+ 
   </div>
 
   <div class="row justify-content-center">
