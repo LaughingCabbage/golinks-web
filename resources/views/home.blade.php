16adbdc8617f4d00bfc8976401ae1314e8793812
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-28 17:18:31 -0500

    prestyle home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-
-@push('stylesheets')
-{{ Html::style('css/home.css') }}
-@endpush
-
-<div class="card bg-dark text-black">
-        <img class="card-img" src="{{asset('img/index-bg.jpg')}}" alt="Card image">
-        <div class="card-img-overlay text-center">
-          <h1 class="card-title">Card title</h1>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text">Last updated 3 mins ago</p>
-          <i class="fa fa-camera-retro fa-lg"></i> fa-lg
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-10">
+      <div class="card text-center">
+        <div class="card-header">
+          Featured
         </div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        <div class="card-footer text-muted">
+          2 days ago
+        </div>
+      </div>
+      <hr class="cubes">
     </div>
+  </div>
 
-<div class="jumbotron jumbotron-fluid">
-    <div class="container text-center">
-        <h1 class="display-3">Fluid jumbotron</h1>
-        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  <div class="row justify-content-center">
+    <div class="col-md-5 mt-3">
+      <div class="card text-center">
+        <div class="card-header">
+          Featured
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        <div class="card-footer text-muted">
+          2 days ago
+        </div>
+      </div>
     </div>
+    <div class="col-md-5 mt-3">
+      <div class="card text-center">
+        <div class="card-header">
+          Featured
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        <div class="card-footer text-muted">
+          2 days ago
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
