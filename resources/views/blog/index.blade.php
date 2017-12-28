c4f05221e2572825a2c7675c1785f80b2b8582cc
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-28 17:25:05 -0500

    center contents remove post route from slug view

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -7,22 +7,18 @@
 @endpush
 
 @section('content')
-<main role="main" class="container">
-    <div class="row">
+<main class="container">
+    <div class="row justify-content-center">
         <div class="col-md-10">
-            <h1>Posts</h1>
-        </div>
-        <div class="col-md-10">
-            <a href="{{ route('post.index') }}" class="btn btn-primary btn-lg btn-block">View All</a>
+            <h1>Recent Posts</h1>
         </div>
     </div>
-    
     <hr>
 
-    <div class="row">
+    <div class="row justify-content-center">
         @foreach($posts as $post)
-        <div class="col-sm-10 blog-main">
-            <div class="card bg-light mb-3" style="max-width: 100rem;">
+        <div class="col-sm-10">
+            <div class="card mb-3" style="max-width: 100rem;">
                 <div class="card-header">{{ date_format($post->created_at, 'M j, Y') }}</div>
                 <div class="card-body">
                     <h1 class="card-title"><a href="{{ route('blog.article', $post->slug) }}">{{ $post->title }}</a></h1>
@@ -31,6 +27,11 @@
             </div>
         </div>        
         @endforeach
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-sm-10">
+            {{ $posts->links('pagination::bootstrap-4') }}    
+        </div>
     </div>
 </main>
 @endsection
