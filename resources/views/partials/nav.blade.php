6fb958b8e1e223bbe30ce2b3d74f8872e0f83de3
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-29 15:00:04 -0500

    fix padding. fix active highlighting

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,28 +1,29 @@
  <div class="container">
-    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-gradient-primary">
-        <a class="navbar-brand" href="/">GoVice</a>
+    <nav class="navbar navbar-expand-sm py-0 fixed-top navbar-dark bg-gradient-primary">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
-
+        <a class="navbar-brand" href="/">
+            <div class="nav-link">GoVice <i class="fa fa-space-shuttle" aria-hidden="true"></i></div>
+        </a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="nav navbar-nav mr-auto">
                 <li class="nav-item active"> {{--Add spacing between site name and buttons --}}
                     <a class="nav-link"><span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item active">{{--Home--}}
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                <li class="nav-item">{{--Home--}}
+                    <a class="nav-link" href="/">Home</a>
                 </li>
-                <li class="nav-item active">{{--Mission--}}
-                    <a class="nav-link" href="/mission">Mission <span class="sr-only">(current)</span></a>
+                <li class="nav-item">{{--Mission--}}
+                    <a class="nav-link" href="/mission">Mission</a>
                 </li>
-                <li class="nav-item active">{{--Blog--}}
-                    <a class="nav-link" href="/blog">Blog <span class="sr-only">(current)</span></a>
+                <li class="nav-item">{{--Blog--}}
+                    <a class="nav-link" href="/blog">Blog</a>
                 </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 @auth
-                <li class="nav-item active dropdown"> {{--Dropdown--}}
+                <li class="nav-item dropdown"> {{--Dropdown--}}
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Account
                     </a>
