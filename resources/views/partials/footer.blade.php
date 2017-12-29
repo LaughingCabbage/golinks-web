0973e030447a782f63f3e572a337bef3c366a5e7
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-12-29 14:55:48 -0500

    add portfolio link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,9 @@
     <div class="container-fluid fixed-bottom">       
         <div class="row bg-gradient-primary">
             <div class="col text-center">
-                <span class="text-muted"><i class="fa fa-copyright" aria-hidden="true"></i>  2018 Kevin Gentile</span>
+                <span class="text-muted"><i class="fa fa-copyright" aria-hidden="true"></i>  2018 
+                    <a class="text-muted" href="http://www.kevingentile.com">Kevin Gentile</a>
+                </span>
             </div>
         </div>
     <div>
